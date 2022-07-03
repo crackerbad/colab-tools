25f4fa0370d8d4056118ee2955e309fdbf128618
crackerbad <anonymouscrackerbad@gmail.com>
2022-07-03 20:03:21 -0300

    Add files via upload

---
+++ colab-tools.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+sudo apt-get update
+
+#npm torrent client
+npm install -g npm
+npm i torrent -g
+
+#Croc Send files
+curl https://getcroc.schollz.com | bash
+
+#youtube-dlp
+sudo wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/local/bin/yt-dlp
+sudo chmod a+rx /usr/local/bin/yt-dlp
+
+#cryptodome for decrypt streams
+pip install pycryptodome
+
+#google drive downloader
+pip install gdown
+
+#ffmpeg
+sudo apt-get install ffmpeg -y
+
+#mkvtoolnix
+sudo apt-get install mkvtoolnix -y
+
+#aria2c
+sudo apt-get install aria2 -y
+
+echo Instalação Finalizada
+
