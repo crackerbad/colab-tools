ffbc5d8d2f1d3cf53c4f1c02eab2b0ea3319a411
crackerbad <anonymouscrackerbad@gmail.com>
2023-05-07 16:06:26 -0300

    Add files via upload

--- tools/cloudflared.sh
+++ tools/cloudflared.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 
-source /etc/env
-export $(sed '/^#/d' /etc/env | cut -d= -f1)
-
-if [ "${GLOBAL_LANGUAGE}" = "chs" ]; then
-    echo "Cloudflared Argo 隧道将于 10 秒至 1 分钟后准备好"
-else
-    echo "Cloudflared Argo Tunnel will be ready in one minute"
-fi
+#source /etc/env
+#export $(sed '/^#/d' /etc/env | cut -d= -f1)
+echo "Cloudflared Argo Tunnel will be ready in one minute"
 
 killall argo 2>/dev/null
+cd /content/tools/
 rm -f nohup.out
-nohup argo tunnel --url http://localhost:8880 &
+nohup argo tunnel --url http://localhost:3000 &
 sleep 5
 
-while [[ "${URL}" = "" ]]; do
-    URL=$(grep -oP "https://.*trycloudflare.com" nohup.out)
-    sleep 3
-done
+URL=$(grep -oh "https://\(.*\)trycloudflare.com" nohup.out)
 
-while [[ "${HTTP_CODE}" -ne 200 ]]; do
-    HTTP_CODE=$(curl -I -s -w %{http_code} ${URL} -o /dev/null)
-    sleep 5
-done
-
-echo ${URL}${GLOBAL_PORTAL_PATH}
-tail -f
+echo Argo Tunnel Iniciado em : ${URL}
+#tail -f
