2b4d01b824c549e7fc9e3ab9fd8255e209955ed6
crackerbad <anonymouscrackerbad@gmail.com>
2023-05-08 14:23:53 -0300

    Add files via upload

--- tools/cloudflared.sh
+++ tools/cloudflared.sh
@@ -15,7 +15,7 @@ echo "Cloudflared Argo Tunnel port $port will be ready in one minute"
 killall argo 2>/dev/null
 cd /content/tools/
 rm -f nohup.out
-nohup argo tunnel --url http://localhost:$port &
+nohup argo tunnel --url localhost:$port &
 sleep 5
 
 URL=$(grep -oh "https://\(.*\)trycloudflare.com" nohup.out)
