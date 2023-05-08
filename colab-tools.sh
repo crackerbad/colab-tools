967c0ad243bbee1f9884f8dac495ef6f54214297
crackerbad <anonymouscrackerbad@gmail.com>
2023-05-08 04:11:54 -0300

    Add files via upload

--- colab-tools.sh
+++ colab-tools.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 mkdir "tools"
 
 INSTALL_CRDOWNLOADER(){
-	cd tools
+	cd "tools"
 	wget "https://github.com/anidl/multi-downloader-nx/releases/download/3.4.0/multi-downloader-nx-ubuntu-gui.7z"
 	7z e "multi-downloader-nx-ubuntu-gui.7z"
 	rm multi-downloader-nx-ubuntu-gui.7z
@@ -12,7 +12,7 @@ INSTALL_CRDOWNLOADER(){
 	wget "https://cdn.discordapp.com/attachments/1092465034103369798/1104601009357070387/config.zip"
 	unzip config.zip
 	rm config.zip
-	cd /content/
+	cd "/content/"
 }
 
 INSTALL_CLOUDFLARED() {
