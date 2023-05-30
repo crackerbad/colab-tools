3fc03f33fa29f05addffac063884ec2ec4c34f47
crackerbad <anonymouscrackerbad@gmail.com>
2023-05-30 09:32:46 -0300

    Add files via upload

--- tools/colab-index-downloader.py
+++ tools/colab-index-downloader.py
@@ -160,11 +160,11 @@ def download_files_from_list():
     try:
         print("\nStarting Downloads with aria2c...")
         list_file = list_folder + '/download_list.txt'
-        # subprocess.run(['aria2c', "--dir=" + OUTPUT_DIR, "--input-file=" + list_file, "--max-concurrent-downloads=3",
-        #    "--connect-timeout=60", "--max-connection-per-server=8", "--continue=true", "--split=8", "--min-split-size=1M",
-        #    "--human-readable=true", "--download-result=full", "--file-allocation=none", "--auto-save-interval=0"])
-        # if os.path.exists(list_file):
-        #    os.remove(list_file)
+        subprocess.run(['aria2c', "--dir=" + OUTPUT_DIR, "--input-file=" + list_file, "--max-concurrent-downloads=3",
+            "--connect-timeout=60", "--max-connection-per-server=8", "--continue=true", "--split=8", "--min-split-size=1M",
+            "--human-readable=true", "--download-result=full", "--file-allocation=none", "--auto-save-interval=0"])
+         if os.path.exists(list_file):
+            os.remove(list_file)
         return True
     except Exception as e:
         print(f"Error downloading files: {e}")
