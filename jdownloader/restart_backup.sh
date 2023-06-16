e622baff9b7d20532bc8da3648aa8790f7766ad2
crackerbad <anonymouscrackerbad@gmail.com>
2023-06-16 02:03:37 -0300

    Add files via upload

--- jdownloader/restart_backup.sh
+++ jdownloader/restart_backup.sh
@@ -3,6 +3,7 @@
 # Nome do script Python a ser encerrado
 nome_script="move_downloads.py"
 
+
 # Obtém o PID do processo Python com base no nome do script
 pid=$(pgrep -f "$nome_script")
 
@@ -10,7 +11,7 @@ pid=$(pgrep -f "$nome_script")
 if [ -z "$pid" ]; then
     echo "O processo $nome_script não está em execução."
     echo "Reiniciando Sistema de Backup..."
-    nohup python /content/tools/jdownloader/move_downloads.py &
+    nohup python /content/tools/jdownloader/move_downloads.py -o "$SAVE_FOLDER" > /content/logs/JDMove_Downloads.log 2>&1 &
 else
     # Processo em execução
     echo "Processo $nome_script (PID: $pid) já está em execução,"
