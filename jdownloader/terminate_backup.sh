fa4c7f79b8ad8d84f7d4c96d2e7766414030dc13
crackerbad <anonymouscrackerbad@gmail.com>
2023-06-17 04:02:29 -0300

    Add files via upload

---
+++ jdownloader/terminate_backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Nome do script Python a ser encerrado
+nome_script="move_downloads.py"
+
+
+# Obtém o PID do processo Python com base no nome do script
+pid=$(pgrep -f "$nome_script")
+
+# Verifica se o processo está em execução
+if [ -z "$pid" ]; then
+    echo "O processo $nome_script não está em execução."
+else
+    # Processo em execução
+    echo "Finalizando processo $nome_script (PID: $pid)."
+    kill "$pid"
+fi
