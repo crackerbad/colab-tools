ee2aea2981edd5ab68c62e317ba07d06f0d7f999
crackerbad <anonymouscrackerbad@gmail.com>
2023-06-18 19:07:01 -0300

    Add files via upload

--- jdownloader/restart_backup.sh
+++ jdownloader/restart_backup.sh
@@ -11,11 +11,11 @@ pid=$(pgrep -f "$nome_script")
 if [ -z "$pid" ]; then
     echo "O processo $nome_script não está em execução."
     echo "Inicializando Sistema de Backup..."
-    nohup python /content/tools/jdownloader/move_downloads.py -o "$SAVE_FOLDER" > /content/logs/JDMove_Downloads.log 2>&1 &
+    nohup python /content/tools/jdownloader/jdownload_to_rclone.py -o "$SAVE_FOLDER" > /content/logs/JDMove_Downloads.log 2>&1 &
 else
     # Processo em execução
     echo "Processo $nome_script (PID: $pid) já está em execução."
     kill "$pid"
-    nohup python /content/tools/jdownloader/move_downloads.py -o "$SAVE_FOLDER" > /content/logs/JDMove_Downloads.log 2>&1 &
+    nohup python /content/tools/jdownloader/jdownload_to_rclone.py -o "$SAVE_FOLDER" > /content/logs/JDMove_Downloads.log 2>&1 &
     echo "Sistema de Backup Reiniciado."
 fi
