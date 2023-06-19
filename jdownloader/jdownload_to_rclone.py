97cb2acd8258b6c5883f7dfad3e374c8708d7fcc
crackerbad <anonymouscrackerbad@gmail.com>
2023-06-19 15:36:30 -0300

    Add files via upload

--- jdownloader/jdownload_to_rclone.py
+++ jdownloader/jdownload_to_rclone.py
@@ -22,7 +22,7 @@ pasta_origem = "/content/downloads/jd2_downloads"
 pasta_destino = args.output
 
 # Extensões de arquivo a serem ignoradas
-extensoes_ignoradas = [".rar", ".7z", ".zip", ".part", ".encrypted", ".py", ".sh"]
+extensoes_ignoradas = [".rar", ".7z", ".zip", ".part", ".encrypted", ".py", ".sh", ".ts"]
 
 # Dicionário para rastrear os tamanhos dos arquivos
 tamanhos_arquivos = {}
