34c07f6affc8b663a56d0f176ff5c29189464bc1
crackerbad <anonymouscrackerbad@gmail.com>
2023-07-15 17:56:56 -0300

    Add files via upload

---
+++ tools/crunchyroll_start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+rm /content/logs/aniDL_tunnel.log
+nohup argo tunnel --url localhost:3030 > /content/logs/aniDL_tunnel.log 2>&1 &
+sleep 5
+CRURL=$(grep -oh "https://\(.*\)trycloudflare.com" /content/logs/aniDL_tunnel.log)
+sed -i "s|CRUNCHYROLL_URL|${CRURL}|" /content/homer/assets/config.yml
