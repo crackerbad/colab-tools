2b9362da7fe4413ebb75cf2a4569e62910bdf52a
crackerbad <anonymouscrackerbad@gmail.com>
2024-10-09 17:27:23 -0300

    Add files via upload

--- tools/crunchyroll-remuxer.sh
+++ tools/crunchyroll-remuxer.sh
@@ -16,16 +16,22 @@ for f in *.mkv; do
     # Verifica o idioma da primeira faixa de áudio
     lang=$(ffprobe -v error -select_streams a:0 -show_entries stream_tags=language -of csv=p=0 "$f")
 
+    # Remove a extensão ".mkv" e a parte "E${episode}" para o nome da pasta
+    folder_name=$(echo "$new_f" | sed -E 's/(.*)(E[0-9]+)(.*)\.mkv/\1\3/')
+
+    # Cria o diretório baseado no nome extraído (se não existir)
+    mkdir -p "$output/$folder_name"
+
     if [ "$lang" == "jpn" ]; then
         echo "Áudio primário japonês, prosseguindo com alterações."
         # Se o idioma for Japonês, corrige e padroniza as propriedades
         mkvpropedit "$f" --edit track:s2 --set flag-forced=1 --edit track:s2 --set name="Brazilian Portuguese Forced"
-        mkvmerge -o "$output/CR/$new_f" "$f" --track-order 0:0,0:2,0:1,0:4,0:3
+        mkvmerge -o "$output/$folder_name/$new_f" "$f" --track-order 0:0,0:2,0:1,0:4,0:3
     else
         echo "Áudio primário não é japonês, prosseguindo com alterações."
         # Se o idioma não for japonês, ajusta o arquivo
         mkvpropedit "$f" --edit track:s1 --set flag-forced=1 --edit track:s1 --set name="Brazilian Portuguese Forced"
-        mkvmerge -o "$output/CR/$new_f" "$f"
+        mkvmerge -o "$output/$folder_name/$new_f" "$f"
     fi
     # Remove o arquivo original
     rm "$f"
--- tools/crunchyroll-remuxer_delayed.sh
+++ tools/crunchyroll-remuxer_delayed.sh
@@ -18,16 +18,22 @@ for f in *.mkv; do
     # Verifica o idioma da primeira faixa de audio
     lang=$(ffprobe -v error -select_streams a:0 -show_entries stream_tags=language -of csv=p=0 "$f")
 
+    # Remove a extensão ".mkv" e a parte "E${episode}" para o nome da pasta
+    folder_name=$(echo "$new_f" | sed -E 's/(.*)(E[0-9]+)(.*)\.mkv/\1\3/')
+
+    # Cria o diretório baseado no nome extraído (se não existir)
+    mkdir -p "$output/$folder_name"
+
     if [ "$lang" == "jpn" ]; then
         echo "Audio primario jápones prosseguindo com alterações."
         # Se o idioma for Português, corrige e padroniza as propriedades
         mkvpropedit "$f" --edit track:s2 --set flag-forced=1 --edit track:s2 --set name="Brazilian Portuguese Forced"
-        mkvmerge -o "$output/CR/$new_f" --sync $audiotrack:$delaytime "$f" --track-order 0:0,0:2,0:1,0:4,0:3
+        mkvmerge -o "$output/$folder_name/$new_f" --sync $audiotrack:$delaytime "$f" --track-order 0:0,0:2,0:1,0:4,0:3
     else
         # Se o idioma não for Português
         echo "Audio primario português prosseguindo com alterações."
         mkvpropedit "$f" --edit track:s1 --set flag-forced=1 --edit track:s1 --set name="Brazilian Portuguese Forced"
-        mkvmerge -o "$output/CR/$new_f" --sync $audiotrack:$delaytime "$f"
+        mkvmerge -o "$output/$folder_name/$new_f" --sync $audiotrack:$delaytime "$f"
     fi
     # Remove o arquivo original
     rm "$f"
